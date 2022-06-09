8d2421f5324ef7097b50331260268b12afd1cfa9
˜Renato˜ <renato.melo@ab-inbev.com>
2022-06-08 23:57:53 -0300

    feat(postgres): add and configure postgres
    install postgres driver to use without orm

--- src/routes/customer.ts
+++ src/routes/customer.ts
@@ -1,8 +1,15 @@
 import { Router } from "express";
+import { client } from "../postgres";
 
 const customerRoutes = Router()
 
 customerRoutes.get('/', (_, res) => {
+  client.connect()
+  client.query('SELECT NOW()', (err, res) => {
+    console.log(err, res)
+    client.end()
+  })
+
   return res.send('GET route for customer!')
 })
 
