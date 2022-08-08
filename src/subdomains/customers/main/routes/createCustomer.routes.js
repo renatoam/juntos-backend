e740a559fc92864cbf01f1d61bb33e5c449ab7e1
Renato <renato.melo@ab-inbev.com>
2022-08-07 23:10:55 -0300

    chore: remove unused and wrong created files

--- src/subdomains/customers/main/routes/createCustomer.routes.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const createCustomer_1 = require("../createCustomer");
-const router = (0, express_1.Router)();
-router.post('/create', createCustomer_1.createCustomerController.execute);
--- src/subdomains/customers/main/routes/createCustomer.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import { createCustomerController } from "../createCustomer";
-
-const router = Router()
-
-router.post('/create', createCustomerController.execute)
