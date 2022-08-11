c3bcb0c3754aa22f26e5af311b5f409c727204cf
Renato <renato.melo@ab-inbev.com>
2022-08-10 21:45:58 -0300

    feat: create create customer controller

--- src/subdomains/customers/infrastructure/controllers/CreateCustomerController.ts
+++ src/subdomains/customers/infrastructure/controllers/CreateCustomerController.ts
@@ -1,37 +1,54 @@
-import { Controller } from "../../../employees/adapters/controllers/BaseController";
-import { CreateCustomerDTO } from "../../adapters/dtos/CreateCustomerDTO";
-import { CreateCustomerUseCase } from "../../useCases/createCustomer";
+import { Request, Response } from "express";
+import { client } from "../../../../shared/infrastructure/database/postgres";
+import { LocationMapper } from "../../../../shared/infrastructure/mappers/LocationMapper";
+import { Customer } from "../../domain/Customer";
+import { CustomCustomerRepository } from "../repositories/CustomCustomerRepository";
 
-export class CreateCustomerController extends Controller {
-  private useCase: CreateCustomerUseCase;
-
-  constructor(useCase: CreateCustomerUseCase) {
-    super();
-    this.useCase = useCase;
-  }
-
-  protected async handle(): Promise<any> {
-    const dto: CreateCustomerDTO = this.request.body as CreateCustomerDTO;
-    return ''
-
-    // try {
-    //   const result = await this.useCase.execute(dto);
-
-    //   if (result.isLeft()) {
-    //     const error = result.value;
-
-    //     switch (error.constructor) {
-    //       case CreateUserErrors.AccountAlreadyExists:
-    //         return this.conflict(error.errorValue().message)
-    //       default:
-    //         return this.fail(error.errorValue().message);
-    //     }
-    //   } else {
-    //     return this.ok(this.res);
-    //   }
-
-    // } catch (err) {
-    //   return this.fail(err)
-    // }
+export class CreateCustomerController {
+  async run(request: Request, response: Response) {
+    const data = request.body as Customer
+    const customerRepository = new CustomCustomerRepository()
+    const doesCustomerExists = await customerRepository.exists(data.email)
+  
+    if (doesCustomerExists) return response.status(409).json({
+      description: 'Conflict',
+      message: 'Customer already exists!'
+    })
+  
+    const location = LocationMapper.toPersistence(data.location)
+    const insertQueryFields = Object.keys(location).join(',')
+    const insertQueryValues = Object.values(location).map(value => `'${value}'`).join(',')
+  
+    const newCustomer = Customer.create({
+      name: data.name,
+      type: 'customers',
+      cell: data.cell,
+      dob: data.dob,
+      email: data.email,
+      gender: data.gender,
+      location: data.location,
+      occupation: data.occupation,
+      phone: data.phone,
+      picture: data.picture,
+      registered: data.registered,
+      role_id: data.role_id
+    })
+  
+    await client
+    .query(`INSERT INTO locations(${insertQueryFields}) VALUES (${insertQueryValues});`)
+  await client
+    .query(`INSERT INTO locations_customers(location_id, customer_id) VALUES ('${location.location_id}', '${newCustomer.id}');`)
+  
+    try {
+      await customerRepository.save(newCustomer)
+      const customer = await customerRepository.getCustomerByEmail(newCustomer.email)
+  
+      return response.status(200).json({
+        message: 'Customer successfully created!',
+        customer
+      })
+    } catch (error) {
+      return response.status(500).json(`Error on creating customer: ${error}`)
+    }
   }
 }
