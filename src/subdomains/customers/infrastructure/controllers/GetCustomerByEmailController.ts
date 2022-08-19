b20940385ca8d84eb5cfa66e0eddc00ee55c5645
Renato <renato.melo@ab-inbev.com>
2022-08-18 22:09:56 -0300

    refactor: map customer to dto on get customer by email

--- src/subdomains/customers/infrastructure/controllers/GetCustomerByEmailController.ts
+++ src/subdomains/customers/infrastructure/controllers/GetCustomerByEmailController.ts
@@ -1,10 +1,12 @@
 import { Request, Response } from "express";
 import { GetCustomerByEmailUseCase } from "../../useCases/getCustomerByEmail";
+import { CustomerMapper } from "../mappers/CustomerMapper";
 
 export class GetCustomerByEmailController {
   async run(request: Request, response: Response) {
     const useCase = new GetCustomerByEmailUseCase()
-    const customer = await useCase.execute(request.params.email)
+    const rawCustomer = await useCase.execute(request.params.email)
+    const customer = CustomerMapper.toDTO(rawCustomer)
 
     return response.status(200).json({ customer })
   }
